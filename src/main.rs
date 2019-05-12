a75b577ae189cdd2d2bf8aa2dd9e07315cb88d8c
Brooks J Rady <b.j.rady@gmail.com>
2019-05-12 11:01:16 +0100

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
+extern crate clap;
 extern crate evdev_rs as evdev;
-extern crate serialport;
 extern crate rand;
-extern crate clap; 
+extern crate serialport;
 
-use evdev::*;
+use clap::{App, Arg};
 use evdev::enums::*;
+use evdev::*;
+use rand::Rng;
+use serialport::prelude::*;
 use std::fs::File;
+use std::io;
+use std::process::Command;
+use std::str::from_utf8;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use std::io;
-use serialport::prelude::*;
-use std::str::from_utf8;
-use rand::Rng;
-use std::process::Command;
-use clap::{App, Arg};
 
 fn press_keys(keys: &Vec<EV_KEY>, dev: &UInputDevice) {
-    let t = TimeVal::new(0,0);
-    let syn = vec![InputEvent::new(&t, &EventCode::EV_SYN(EV_SYN::SYN_REPORT), 0)];
-    let events = |val| keys.iter()
-        .map(|key| InputEvent::new(&t, &EventCode::EV_KEY(key.clone()), val))
-        .collect::<Vec<InputEvent>>();
-    for e in vec![events(1),syn.clone(),events(0),syn].into_iter().flatten() {
+    let t = TimeVal::new(0, 0);
+    let syn = vec![InputEvent::new(
+        &t,
+        &EventCode::EV_SYN(EV_SYN::SYN_REPORT),
+        0,
+    )];
+    let events = |val| {
+        keys.iter()
+            .map(|key| InputEvent::new(&t, &EventCode::EV_KEY(key.clone()), val))
+            .collect::<Vec<InputEvent>>()
+    };
+    for e in vec![events(1), syn.clone(), events(0), syn]
+        .into_iter()
+        .flatten()
+    {
         dev.write_event(&e).expect("Scheiße");
     }
 }
@@ -32,34 +41,47 @@ fn memes(dev: &UInputDevice) {
     let mut rng = rand::thread_rng();
     for _ in 0..power {
         vol_up(&dev);
-        if rng.gen() {up_space(&dev)} else {down_space(&dev)}
+        if rng.gen() {
+            up_space(&dev)
+        } else {
+            down_space(&dev)
+        }
         sleep(t);
     }
     play_pause(&dev);
 }
 
 fn down_space(dev: &UInputDevice) {
-    press_keys(&vec![EV_KEY::KEY_LEFTCTRL,EV_KEY::KEY_LEFTALT,EV_KEY::KEY_DOWN],&dev);
+    press_keys(
+        &vec![EV_KEY::KEY_LEFTCTRL, EV_KEY::KEY_LEFTALT, EV_KEY::KEY_DOWN],
+        &dev,
+    );
 }
 
 fn up_space(dev: &UInputDevice) {
-    press_keys(&vec![EV_KEY::KEY_LEFTCTRL,EV_KEY::KEY_LEFTALT,EV_KEY::KEY_UP],&dev);
+    press_keys(
+        &vec![EV_KEY::KEY_LEFTCTRL, EV_KEY::KEY_LEFTALT, EV_KEY::KEY_UP],
+        &dev,
+    );
 }
 
 fn play_pause(dev: &UInputDevice) {
-    press_keys(&vec![EV_KEY::KEY_PLAYPAUSE],&dev);
+    press_keys(&vec![EV_KEY::KEY_PLAYPAUSE], &dev);
 }
 
 fn vol_up(dev: &UInputDevice) {
-    press_keys(&vec![EV_KEY::KEY_VOLUMEUP],&dev);
+    press_keys(&vec![EV_KEY::KEY_VOLUMEUP], &dev);
 }
 
 fn vol_down(dev: &UInputDevice) {
-    press_keys(&vec![EV_KEY::KEY_VOLUMEDOWN],&dev);
+    press_keys(&vec![EV_KEY::KEY_VOLUMEDOWN], &dev);
 }
 
 fn launch_terminal(dev: &UInputDevice) {
-    press_keys(&vec![EV_KEY::KEY_LEFTCTRL,EV_KEY::KEY_LEFTALT,EV_KEY::KEY_T],&dev);
+    press_keys(
+        &vec![EV_KEY::KEY_LEFTCTRL, EV_KEY::KEY_LEFTALT, EV_KEY::KEY_T],
+        &dev,
+    );
 }
 
 fn lights_on() {
@@ -75,14 +97,18 @@ fn main() {
         .version("1.0.0")
         .author("Brooks J Rady <b.j.rady@gmail.com>")
         .about("Userspace driver and macro configuration tool for the QiDeck")
-        .arg(Arg::with_name("keyboard")
-             .index(1)
-             .value_name("/dev/input/eventX")
-             .help("Points the program to a keyboard device to emulate"))
-        .arg(Arg::with_name("serial")
-             .index(2)
-             .value_name("/dev/ttyUSBX")
-             .help("Points the program to the serial port of the QiDeck"))
+        .arg(
+            Arg::with_name("keyboard")
+                .index(1)
+                .value_name("/dev/input/eventX")
+                .help("Points the program to a keyboard device to emulate"),
+        )
+        .arg(
+            Arg::with_name("serial")
+                .index(2)
+                .value_name("/dev/ttyUSBX")
+                .help("Points the program to the serial port of the QiDeck"),
+        )
         .get_matches();
     let path = args.value_of("keyboard").unwrap_or("/dev/input/event2");
     let port = args.value_of("serial").unwrap_or("/dev/ttyUSB0");
@@ -108,7 +134,9 @@ fn main() {
                 match p.read(serial_buf.as_mut_slice()) {
                     Ok(n) if tick.elapsed().as_millis() >= 200 => {
                         let digit_str = from_utf8(&serial_buf[..n]).unwrap().trim();
-                        if digit_str.is_empty() { continue; }
+                        if digit_str.is_empty() {
+                            continue;
+                        }
                         match digit_str.parse::<u8>() {
                             Ok(1) => up_space(&i),
                             Ok(2) => vol_up(&i),
@@ -122,7 +150,7 @@ fn main() {
                             _ => println!("{:?}", digit_str),
                         };
                         tick = Instant::now();
-                    },
+                    }
                     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
                     Err(e) => eprintln!("{:?}", e),
                     _ => (),
